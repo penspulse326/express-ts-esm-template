7fb5e79d62dcf49dee95273ed9c4ae6e3a7213fc
SHIN <akiyachiumi@gmail.com>
2024-11-02 08:45:01 +0800

    add ejs and basic structure

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// ES Module 中 __dirname 的替代方案
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 設定 EJS 模板引擎
+app.set("views", path.join(__dirname, "../views")); // 設定視圖目錄
+app.set("view engine", "ejs"); // 設定視圖引擎為 EJS
+
+// 設定靜態檔案目錄
+app.use(express.static(path.join(__dirname, "../public")));
+
 type HomeResponse = {
+  title: string;
   message: string;
 };
 
 app.get("/", (req, res) => {
   const helloMessage: HomeResponse = {
-    message: "Hello Express!",
+    title: "Express + TypeScript + EJS",
+    message: "Hello, World!",
   };
-  res.json(helloMessage);
+  res.render("index", helloMessage);
 });
 
 app.listen(port, () => {
